0db8b7dcc83b904fe143f27c40381d81f328df96
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2016-04-07 13:29:43 +0900

    add tests, writer, travis.yml

--- shapeio.go
+++ shapeio.go
@@ -5,48 +5,88 @@ import (
 	"time"
 )
 
-var zeroTime time.Time
-
-type ShapeIOReader struct {
-	reader      io.Reader
+type limiter struct {
 	bytesPerSec float64
-	start       time.Time
-	readBytes   int64
+	startedAt   time.Time
+	bytes       int64
 }
 
-func NewReader(reader io.Reader) *ShapeIOReader {
-	s := &ShapeIOReader{
-		reader: reader,
-	}
-	return s
+type Reader struct {
+	r io.Reader
+	l *limiter
 }
 
-func (s *ShapeIOReader) SetRateLimit(r float64) {
-	s.bytesPerSec = r
+type Writer struct {
+	w io.Writer
+	l *limiter
 }
 
-func (s *ShapeIOReader) Read(b []byte) (int, error) {
-	if s.bytesPerSec == 0 {
-		return s.reader.Read(b)
+// NewReader returns a reader that implements io.Reader with rate limiting.
+func NewReader(r io.Reader) *Reader {
+	return &Reader{
+		r: r,
+		l: &limiter{},
 	}
-	if s.start.Equal(zeroTime) {
-		s.start = time.Now()
+}
+
+// NewWriter returns a writer that implements io.Writer with rate limiting.
+func NewWriter(w io.Writer) *Writer {
+	return &Writer{
+		w: w,
+		l: &limiter{},
 	}
-	n, err := s.reader.Read(b)
+}
+
+// SetRateLimit sets rate limit (bytes/sec) to the reader.
+func (s *Reader) SetRateLimit(l float64) {
+	s.l.bytesPerSec = l
+}
+
+// Read reads bytes into p.
+func (s *Reader) Read(p []byte) (int, error) {
+	if !s.l.start() {
+		return s.r.Read(p)
+	}
+	return s.l.wait(s.r.Read(p))
+}
+
+// SetRateLimit sets rate limit (bytes/sec) to the writer.
+func (s *Writer) SetRateLimit(l float64) {
+	s.l.bytesPerSec = l
+}
+
+// Write writes bytes from p.
+func (s *Writer) Write(p []byte) (int, error) {
+	if !s.l.start() {
+		return s.w.Write(p)
+	}
+	return s.l.wait(s.w.Write(p))
+}
+
+func (l *limiter) start() bool {
+	if l.bytesPerSec == 0 {
+		return false
+	}
+	if l.startedAt.IsZero() {
+		l.startedAt = time.Now()
+	}
+	return true
+}
+
+func (l *limiter) wait(n int, err error) (int, error) {
 	if n == 0 || err != nil {
 		return n, err
 	}
-	duration := time.Since(s.start)
-	s.readBytes += int64(n)
-
-	rate := float64(s.readBytes) / duration.Seconds()
-	if rate < s.bytesPerSec {
-		return n, err
+	elapsed := time.Since(l.startedAt)
+	l.bytes += int64(n)
+	rate := float64(l.bytes) / elapsed.Seconds()
+	if rate < l.bytesPerSec {
+		return n, nil
 	}
-	d := time.Duration(float64(s.readBytes)/s.bytesPerSec*float64(time.Second) - float64(duration))
+	d := time.Duration(float64(l.bytes)/l.bytesPerSec*float64(time.Second) - float64(elapsed))
 	time.Sleep(d)
 	// reset shaping window
-	s.start = time.Now()
-	s.readBytes = 0
-	return n, err
+	l.startedAt = time.Now()
+	l.bytes = 0
+	return n, nil
 }
---
+++ shapeio_test.go
@@ -0,0 +1,104 @@
+package shapeio_test
+
+import (
+	"bytes"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/dustin/go-humanize"
+	"github.com/fujiwara/shapeio"
+)
+
+var rates = []float64{
+	500 * 1024,       // 500KB/sec
+	1024 * 1024,      // 1MB/sec
+	10 * 1024 * 1024, // 10MB/sec
+	50 * 1024 * 1024, // 50MB/sec
+}
+
+var srcs = []*bytes.Reader{
+	bytes.NewReader(bytes.Repeat([]byte{0}, 64*1024)),   // 64KB
+	bytes.NewReader(bytes.Repeat([]byte{1}, 256*1024)),  // 256KB
+	bytes.NewReader(bytes.Repeat([]byte{2}, 1024*1024)), // 1MB
+}
+
+func ExampleReader() {
+	// example for downloading http body with rate limit.
+	resp, _ := http.Get("http://example.com")
+	defer resp.Body.Close()
+
+	reader := shapeio.NewReader(resp.Body)
+	reader.SetRateLimit(1024 * 10) // 10KB/sec
+	io.Copy(ioutil.Discard, reader)
+}
+
+func ExampleWriter() {
+	// example for writing file with rate limit.
+	src := bytes.NewReader(bytes.Repeat([]byte{0}, 32*1024)) // 32KB
+	f, _ := os.Create("/tmp/foo")
+	writer := shapeio.NewWriter(f)
+	writer.SetRateLimit(1024 * 10) // 10KB/sec
+	io.Copy(writer, src)
+	f.Close()
+}
+
+func TestRead(t *testing.T) {
+	for _, src := range srcs {
+		for _, limit := range rates {
+			src.Seek(0, 0)
+			sio := shapeio.NewReader(src)
+			sio.SetRateLimit(limit)
+			start := time.Now()
+			n, err := io.Copy(ioutil.Discard, sio)
+			elapsed := time.Since(start)
+			if err != nil {
+				t.Errorf("io.Copy failed", err)
+			}
+			realRate := float64(n) / elapsed.Seconds()
+			if realRate > limit {
+				t.Errorf("Limit %f but real rate %f", limit, realRate)
+			}
+			log.Printf(
+				"read %s / %s: Real %s/sec Limit %s/sec. (%f %%)",
+				humanize.IBytes(uint64(n)),
+				elapsed,
+				humanize.IBytes(uint64(realRate)),
+				humanize.IBytes(uint64(limit)),
+				realRate/limit*100,
+			)
+		}
+	}
+}
+
+func TestWrite(t *testing.T) {
+	for _, src := range srcs {
+		for _, limit := range rates {
+			src.Seek(0, 0)
+			sio := shapeio.NewWriter(ioutil.Discard)
+			sio.SetRateLimit(limit)
+			start := time.Now()
+			n, err := io.Copy(sio, src)
+			elapsed := time.Since(start)
+			if err != nil {
+				t.Errorf("io.Copy failed", err)
+			}
+			realRate := float64(n) / elapsed.Seconds()
+			if realRate > limit {
+				t.Errorf("Limit %f but real rate %f", limit, realRate)
+			}
+			log.Printf(
+				"write %s / %s: Real %s/sec Limit %s/sec. (%f %%)",
+				humanize.IBytes(uint64(n)),
+				elapsed,
+				humanize.IBytes(uint64(realRate)),
+				humanize.IBytes(uint64(limit)),
+				realRate/limit*100,
+			)
+		}
+	}
+}
